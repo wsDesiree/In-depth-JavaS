81b0701f02e76704f46f140afab47fc6ccf0ed93
wsDesiree <110789227+wsDesiree@users.noreply.github.com>
2022-09-07 12:46:22 -0500

    arrays // this one still has an error

---
+++ arrays-example.js
@@ -0,0 +1,20 @@
+// let brotherOne = "Johnny";
+// let brotherTwo = "Jimmy";
+// let brotherThree = "James";
+
+const brothers = [
+    {
+        firstName:"Johnny",
+        weight: 150
+    },
+    {
+        firstName: "Jimmy",
+        weight: 160
+    } ,
+    {
+        firstName: "James"
+        weight: 170
+    }
+];
+
+console.log(`We are three brother: ${brothers[0].firstName}, ${brothers[1].firstName}, ${brothers[2].firstName}`);
